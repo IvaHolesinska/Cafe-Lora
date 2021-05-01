df7e5a53b452ec1ef2a780b6ded5298731299b53
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-01 12:33:34 +0200

    interactive navbar -add event listener to all a

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import './style.css';
 
+// add event listener to navBtn
 const navElm = document.querySelector('nav');
 const navBtnElm = document.querySelector('#nav-btn');
 
 navBtnElm.addEventListener('click', () => {
   navElm.classList.toggle('nav-closed');
 });
+
+// add event listener to all a
+const closeNavbar = () => {
+  navElm.classList.add('nav-closed');
+};
+
+const navAllElm = navElm.querySelectorAll('a');
+
+for (let i = 0; i < navAllElm.length; i++) {
+  navAllElm[i].addEventListener('click', closeNavbar);
+}
