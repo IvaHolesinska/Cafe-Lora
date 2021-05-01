a0859ede3842fc0cfdd2c3fb181ec9ed15b51cbd
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-01 12:55:01 +0200

    order - changing Objednat to  Zrušit and adding drink__cup--selected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-
+// ----------   3 ---------
 // add event listener to navBtn
 const navElm = document.querySelector('nav');
 const navBtnElm = document.querySelector('#nav-btn');
@@ -18,3 +18,22 @@ const navAllElm = navElm.querySelectorAll('a');
 for (let i = 0; i < navAllElm.length; i++) {
   navAllElm[i].addEventListener('click', closeNavbar);
 }
+
+// -----------   4   -------------
+const orderBtnElm = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+let ordered = false;
+
+const order = () => {
+  if (ordered === false) {
+    orderBtnElm.textContent = 'Zrušit';
+    drinkCupElm.classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderBtnElm.textContent = 'Objednat';
+    drinkCupElm.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+};
+
+orderBtnElm.addEventListener('click', order);
